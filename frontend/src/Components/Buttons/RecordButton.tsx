029b188469d07f120c7f7c95041ac77bc27e2bfb
Marcosdo666 <marcos.de.oliveira.madeira@gmail.com>
2023-05-18 20:53:24 -0300

    record button style

--- frontend/src/Components/Buttons/RecordButton.tsx
+++ frontend/src/Components/Buttons/RecordButton.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import styles from "./Button.module.css";
+import RecordIcon from "../../Assets/images/record_icon.svg";
+import Image from "next/image";
+import localFont from "next/font/local";
 
 interface RecordButtonProps {
   onClick?: () => void;
 }
 
+const mozaicFont = localFont({
+  src: [
+    {
+      path: "../../assets/fonts/ceibalmozaic-regular-webfont.woff2",
+      style: "normal",
+    },
+  ],
+});
+
 const RecordButton: React.FC<RecordButtonProps> = ({ onClick }) => {
   return (
-    <button type="submit" className={`${styles.button} row`} onClick={onClick}>
-      <div className={styles["record-fill"]}></div>
-      <div>Grabar</div>
+    <button type="submit" className={`${styles.button} ${styles.big} row`} onClick={onClick}>
+      <div>
+        <Image src={RecordIcon} alt=""></Image>
+      </div>
+      <div style={{fontFamily: mozaicFont.style.fontFamily}}>
+        Grabar
+      </div>
     </button>
   );
 };
