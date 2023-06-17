f2fc442f7192ccf2111d235a6b6b2edadee9f985
Marcosdo666 <marcos.de.oliveira.madeira@gmail.com>
2023-06-17 16:14:48 -0300

    boton continuar

---
+++ frontend/src/components/buttons/RoundedButton.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./Button.module.css";
+import localFont from "next/font/local";
+
+interface RoundedButtonProps {
+  onClick?: () => void;
+  variant: keyof Object;
+  children: string | JSX.Element | JSX.Element[];
+}
+
+const mozaicFont = localFont({
+  src: [
+    {
+      path: "../../assets/fonts/ceibalmozaic-regular-webfont.woff2",
+      style: "normal",
+    },
+  ],
+});
+
+const RoundedButton: React.FC<RoundedButtonProps> = ({ children, variant, onClick }) => {
+  const classVariants = {
+    black: styles["black"],
+  };
+  return (
+    <button
+      onClick={onClick}
+      className={`${styles.rounded} ${classVariants[variant]} row`}
+      style={{ fontFamily: mozaicFont.style.fontFamily }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default RoundedButton;
--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
+import Head from "next/head";
 import styles from "./login.module.css";
 import Image from "next/image";
-import LogoCeibal from "../../assets/images/logo_ceibal.png"
+import LogoCeibal from "../../assets/images/logo_ceibal.png";
+import RightIcon from "../../assets/images/right_icon.svg";
 import InputText from "@/components/inputs/InputText";
+import RoundedButton from "@/components/buttons/RoundedButton";
 
 const Login: React.FC = () => {
   useEffect(() => {
@@ -11,15 +14,24 @@ const Login: React.FC = () => {
   }, []);
 
   return (
-    <div className={`${styles["login-layout"]} row`}>
-      <div className={`${styles["col"]}`}>
-        <Image src={LogoCeibal} alt=""></Image>
-        <p>Inicio de Sesión</p>
-        <InputText placeholder="Documento"></InputText>
-        <InputText placeholder="Contraseña"></InputText>
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <div className={`${styles["login-layout"]} row`}>
+        <div className={`${styles["col"]}`}>
+          <Image src={LogoCeibal} alt=""></Image>
+          <p>Inicio de Sesión</p>
+          <InputText placeholder="Documento"></InputText>
+          <InputText placeholder="Contraseña"></InputText>
+          <RoundedButton variant={"black" as keyof Object}>
+            <div>Continuar</div>
+            <Image src={RightIcon} alt=""></Image>
+          </RoundedButton>
+        </div>
+        <div></div>
       </div>
-      <div></div>
-    </div>
+    </>
   );
 };
 
