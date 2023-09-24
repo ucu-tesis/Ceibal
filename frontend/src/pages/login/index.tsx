8c9ca5bdfe2e7436ba04fa4f3ecd976f29b2461a
Joaquín Cuitiño <joaquin@snack.onl>
2023-09-24 16:36:46 -0300

    Cambios en pantalla de login

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -4,16 +4,31 @@ import styles from "./login.module.css";
 import Image from "next/image";
 import LogoCeibal from "../../assets/images/logo_ceibal.png";
 import RightIcon from "../../assets/images/right_icon.svg";
-import InputText from "@/components/inputs/InputText";
-import InputPassword from "@/components/inputs/InputPassword";
+import RightIconBlack from "../../assets/images/right_icon_black.svg";
 import RoundedButton from "@/components/buttons/RoundedButton";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Login: React.FC = () => {
   useEffect(() => {
     document.getElementById("header")?.remove();
     document.querySelector("hr")?.remove();
   }, []);
+
+  const router = useRouter();
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+  const goToGoogleAuth = () => {
+    router.push(`${backendUrl}/auth/google`);
+  };
+
+  const goToFakeStudentLogin = () => {
+    router.push(`${backendUrl}/auth/fake-student`);
+  };
+
+  const goToFakeTeacherLogin = () => {
+    router.push(`${backendUrl}/auth/fake-teacher`);
+  };
 
   return (
     <>
@@ -24,19 +39,25 @@ const Login: React.FC = () => {
         <div className={`${styles["col"]}`}>
           <Image src={LogoCeibal} alt=""></Image>
           <p>Inicio de Sesión</p>
-          <RoundedButton variant="white">
-            <div>Acceder con Google</div>
-            <Image src={RightIcon} alt=""></Image>
+          <RoundedButton variant="white" onClick={goToGoogleAuth}>
+            <div>Ingresar con Google</div>
+            <Image src={RightIconBlack} alt="" aria-hidden="true"></Image>
           </RoundedButton>
-          <RoundedButton variant="black">
+          <RoundedButton variant="black" onClick={goToFakeStudentLogin}>
             <div>Login de prueba: Estudiante</div>
-            <Image src={RightIcon} alt=""></Image>
+            <Image src={RightIcon} alt="" aria-hidden="true"></Image>
           </RoundedButton>
-          <RoundedButton variant="black">
+          <RoundedButton variant="black" onClick={goToFakeTeacherLogin}>
             <div>Login de prueba: Maestro</div>
-            <Image src={RightIcon} alt=""></Image>
+            <Image src={RightIcon} alt="" aria-hidden="true"></Image>
           </RoundedButton>
-          <span>Si aún no te has registrado como usuario/a y quieres acceder a las plataformas, haz <Link href="https://aulas.ceibal.edu.uy/registrarme">clic aquí</Link></span>
+          <span>
+            Si aún no te has registrado como usuario/a y quieres acceder a las
+            plataformas, haz{" "}
+            <Link href="https://aulas.ceibal.edu.uy/registrarme">
+              clic aquí
+            </Link>
+          </span>
         </div>
         <div></div>
       </div>
