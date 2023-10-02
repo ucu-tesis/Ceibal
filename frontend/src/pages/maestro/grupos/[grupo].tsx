9f0efb772f05fd3a58a94fd60b55d14fc3c48e80
Marcosdo666 <42185858+marcos-do@users.noreply.github.com>
2023-10-02 20:13:22 -0300

    modal crear tarea

--- frontend/src/pages/maestro/grupos/[grupo].tsx
+++ frontend/src/pages/maestro/grupos/[grupo].tsx
@@ -2,9 +2,21 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  ModalContent,
+  ModalCloseButton,
+  ModalBody,
+  Checkbox,
+  ModalFooter,
+} from "@chakra-ui/react";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, Input, InputGroup, InputRightAddon } from "@chakra-ui/react";
-import { SearchIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { SearchIcon, ChevronRightIcon, AddIcon } from "@chakra-ui/icons";
 import ChakraTable, { ChakraTableColumn } from "@/components/tables/ChakraTable";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import styles from "./grupos.module.css";
@@ -29,6 +41,13 @@ const columns: ChakraTableColumn[] = [
 ];
 
 const taskColumns: ChakraTableColumn[] = [{ label: "Sección" }, { label: "Capítulo" }, { label: "Lectura" }];
+
+const taskColumnsModal: ChakraTableColumn[] = [
+  { label: "" },
+  { label: "Sección" },
+  { label: "Capítulo" },
+  { label: "Lectura" },
+];
 
 const taskList: Task[] = [
   { chapter: "4", section: "6", reading: "Coco Bandini" },
@@ -55,6 +74,26 @@ const toTableList = (students: StudentWithFullName[], groupId: number, groupName
     ),
   }));
 
+const toTableListTask = (tasks: Task[]) =>
+  tasks.map((task) => ({
+    ...task,
+    link: (
+      <Link
+        href={{
+          pathname: "#",
+        }}
+      >
+        Ver detalles
+      </Link>
+    ),
+  }));
+
+const toTableListModal = (tasks: Task[]) =>
+  tasks.map((task) => ({
+    checkbox: <Checkbox />,
+    ...task,
+  }));
+
 export default function Page({ params }: { params: { grupo: number } }) {
   const { query } = useRouter();
   const groupId = query.grupo;
@@ -62,6 +101,8 @@ export default function Page({ params }: { params: { grupo: number } }) {
   const [searchQuery, setSearchQuery] = useState("");
   const { groupName, students } = data ?? { groupName: "", students: [] };
   const { filteredStudents } = useFilteredStudents(students ?? [], searchQuery);
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   const inputRegex = /\w|\d|\-|\s/;
 
@@ -91,7 +132,12 @@ export default function Page({ params }: { params: { grupo: number } }) {
             <BreadcrumbLink href="#">{groupName}</BreadcrumbLink>
           </BreadcrumbItem>
         </Breadcrumb>
-        <h1 tabIndex={0}>{groupName}</h1>
+        <div className={`${styles.space} row`}>
+          <h1 tabIndex={0}>{groupName}</h1>
+          <Button onClick={onOpen} leftIcon={<AddIcon />} className={styles.primary} variant="solid">
+            Agregar tareas
+          </Button>
+        </div>
         <Tabs>
           <TabList>
             <Tab>Alumnos</Tab>
@@ -143,11 +189,51 @@ export default function Page({ params }: { params: { grupo: number } }) {
                   </InputRightAddon>
                 </InputGroup>
               </div>
-              <ChakraTable variant="simple" columns={taskColumns} data={taskList}></ChakraTable>
+              <ChakraTable columns={taskColumns} data={toTableListTask(taskList)}></ChakraTable>
             </TabPanel>
           </TabPanels>
         </Tabs>
       </div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent minWidth="700px">
+          <ModalHeader>Crear Tarea</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div className={`${styles.desc} col`}>
+              <div className="row">
+                <span>Fecha límite:</span>
+              </div>
+              <div className="row">
+                <span>Textos:</span>
+              </div>
+            </div>
+            <div className={`${styles.filters} row`}>
+              <InputGroup>
+                <Input
+                  width="auto"
+                  onKeyDown={(e) => {
+                    if (!e.key.match(inputRegex)) {
+                      e.preventDefault();
+                    }
+                  }}
+                  maxLength={30}
+                  placeholder="Lectura"
+                />
+                <InputRightAddon>
+                  <SearchIcon />
+                </InputRightAddon>
+              </InputGroup>
+            </div>
+            <ChakraTable variant="simple" columns={taskColumnsModal} data={toTableListModal(taskList)}></ChakraTable>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose} className={styles.primary} variant="solid">
+              Crear tarea
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </ChakraProvider>
   );
 }
