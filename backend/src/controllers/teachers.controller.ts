2ee2e449b6115be110396cf2ec9deee1791e091f
Joaquín <joaco@hey.com>
2023-10-29 17:07:34 -0300

    Detalle de una evaluación para maestros

--- backend/src/controllers/teachers.controller.ts
+++ backend/src/controllers/teachers.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { TeacherGuard } from 'src/guards/teacher.guard';
 import { PrismaService } from 'src/prisma.service';
 import { Pagination } from 'src/decorators/pagination.decorator';
+import { UserService } from 'src/services/user.service';
+import { RecordingDetailResponse } from 'src/models/recording-detail-response.model';
 
 @Controller('teachers')
 export class TeachersController {
-  constructor(private prismaService: PrismaService) {}
+  constructor(private prismaService: PrismaService, private userService: UserService) { }
 
   @Get('/')
   @UseGuards(TeacherGuard)
@@ -23,5 +25,45 @@ export class TeachersController {
         return user;
       }),
     };
+  }
+
+  @Get('/recordings/:id')
+  @UseGuards(TeacherGuard)
+  async getAnalysis(
+    @Param('id') id: String,
+  ): Promise<RecordingDetailResponse> {
+    const user = this.userService.get();
+    const recording = await this.prismaService.recording.findFirstOrThrow({
+      where: {
+        id: Number(id),
+        EvaluationGroupReading: {
+          EvaluationGroup: {
+            teacher_id: user.id,
+          },
+        },
+      },
+      include: {
+        Analysis: true,
+        Student: true,
+        EvaluationGroupReading: {
+          include: {
+            Reading: true,
+          },
+        },
+      },
+    });
+    return {
+      analysis_id: recording.Analysis[0].id,
+      student_id: recording.student_id,
+      student_first_name: recording.Student.first_name,
+      student_last_name: recording.Student.last_name,
+      reading_id: recording.EvaluationGroupReading.Reading.id,
+      reading_title: recording.EvaluationGroupReading.Reading.title,
+      category: recording.EvaluationGroupReading.Reading.category,
+      subcategory: recording.EvaluationGroupReading.Reading.subcategory,
+      metrics: {
+        // TODO: No definido aun.
+      }
+    }
   }
 }
