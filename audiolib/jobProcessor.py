24a6ec6bf466a768cb4f86369e2e3fb78b3d4a90
farchi (Alexis Dotta) <farchiten@gmail.com>
2023-12-08 19:35:17 -0300

    Tweaks to upload recording endpoint - make analysis fields nullable - create pending analysis row on recording upload - attach recording to proper evaluation group reading

--- audiolib/jobProcessor.py
+++ audiolib/jobProcessor.py
@@ -3,19 +3,19 @@ import requests
 from helpers import save_data, getImageAndJson
 # from analizar_audio import analisis_audio
 
-# TODO conn.close() ?
-conn = psycopg2.connect(
-    host="localhost",
-    database="ceibal_development",
-    user="postgres",
-    password="password",
-    port="5433"
-)
+# TODO get this from env
+def get_db_connection():
+    return psycopg2.connect(
+        host="localhost",
+        database="ceibal_development",
+        user="postgres",
+        password="password",
+        port="5433"
+    )
 
-def grab_pending_row():
-    # TODO add WORKING + stale
-    # TODO add retrying failed jobs
-    
+# TODO add WORKING + stale
+# TODO add retrying failed jobs
+def grab_pending_row():    
     # source for query: https://dba.stackexchange.com/questions/69471/postgres-update-limit-1
     # this is for supporting concurrency between different processors
     sql = """
@@ -32,15 +32,14 @@ def grab_pending_row():
     """
     updated_row_id = None
     try:
-        cur = conn.cursor()
-        cur.execute(sql)
-        
-        updated_rows = cur.rowcount
-        if updated_rows > 0:
-            updated_row_id = cur.fetchone()[0]
-        
-        conn.commit()
-        cur.close()
+        with get_db_connection() as conn, conn.cursor() as cur:
+            cur.execute(sql)
+            
+            updated_rows = cur.rowcount
+            if updated_rows > 0:
+                updated_row_id = cur.fetchone()[0]
+            
+            conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print("query error:")
         print(error)
@@ -58,12 +57,10 @@ def get_recording_data(analysis_id):
     """
     recording_data = None
     try:
-        cur = conn.cursor()
-        cur.execute(sql, (analysis_id,))
-        
-        recording_data = cur.fetchone()
-        
-        cur.close()
+        with get_db_connection() as conn, conn.cursor() as cur:
+            cur.execute(sql, (analysis_id,))
+            
+            recording_data = cur.fetchone()        
     except (Exception, psycopg2.DatabaseError) as error:
         print("query error:")
         print(error)
@@ -93,6 +90,29 @@ def process_row(analysis_id):
     # resJson, img1, img2, img3, img4 = getImageAndJson(newDir)
 
     # update analysis row using data from resJson
+
+    # if (data) {
+    #   const stats = data.indicadores;
+    #   await this.prismaService.analysis.create({
+    #     data: {
+    #       status: AnalysisStatus.COMPLETED,
+    #       repetitions_count: stats.cantidad_de_repeticiones,
+    #       silences_count: stats.cantidad_de_silencios,
+    #       allosaurus_general_error: stats.error_general_allosaurus,
+    #       similarity_error: stats.error_similitud,
+    #       repeated_phonemes: stats.fonemas_repetidos,
+    #       words_with_errors: stats.palabras_con_errores,
+    #       words_with_repetitions: stats.palabras_con_repeticiones,
+    #       score: stats.puntaje,
+    #       error_timestamps: stats.tiempo_errores,
+    #       repetition_timestamps: stats.tiempo_repeticiones,
+    #       phoneme_velocity: Number(stats.velocidad_fonemas) || 0,
+    #       words_velocity: Number(stats.velocidad_palabras) || 0,
+    #       raw_analysis: data,
+    #       recording_id: recording.id,
+    #     },
+    #   });
+    # }
 
 # TODO make this into a loop
 updated_row_id = grab_pending_row()
--- backend/src/services/file-upload.service.ts
+++ backend/src/services/file-upload.service.ts
@@ -7,8 +7,6 @@ import {
 } from '@nestjs/platform-express';
 import { diskStorage, File } from 'multer';
 import { PrismaService } from 'src/prisma.service';
-import axios from 'axios';
-import { AnalysisStatus } from '@prisma/client';
 
 @Injectable()
 export class FileUploadService implements MulterOptionsFactory {
@@ -40,10 +38,7 @@ export class FileUploadService implements MulterOptionsFactory {
     };
   }
 
-  async uploadFileToS3(
-    file: File,
-    studentId: number,
-  ): Promise<{ path: string; data: any }> {
+  async uploadFileToS3(file: File): Promise<string> {
     const bucket = this.configService.get('AWS_BUCKET');
     const filename = `${Date.now()}-${file.originalname}`;
     try {
@@ -55,74 +50,14 @@ export class FileUploadService implements MulterOptionsFactory {
       };
       const command = new PutObjectCommand(params);
       await this.s3.send(command);
+      const path = `https://${bucket}.s3.${this.configService.get(
+        'AWS_REGION',
+      )}.amazonaws.com/${filename}`;
+
+      return path;
     } catch (error) {
       console.log(error);
       throw new BadRequestException('Error uploading file to S3');
     }
-    const path = `https://${bucket}.s3.${this.configService.get(
-      'AWS_REGION',
-    )}.amazonaws.com/${filename}`;
-
-    const evaluationGroupReading =
-      await this.prismaService.evaluationGroupReading.findFirst();
-
-    const formData = new FormData();
-    formData.append('text', 'some text'); // TODO set proper text
-    formData.append('file', new Blob([file.buffer]), file.originalname);
-
-    // TODO add typing to `data`
-    let data = null;
-    try {
-      const response = await axios.post(
-        `${this.configService.get('AUDIOLIB_URL')}/process_audio`,
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        },
-      );
-      data = response.data;
-    } catch (error) {
-      console.log('Unable to process audio');
-      console.error(error.message);
-      console.error(error?.response?.data); // TODO log axios error properly
-    }
-
-    // TODO add created_at to recording
-    const recording = await this.prismaService.recording.create({
-      data: {
-        recording_url: path,
-        student_id: studentId,
-        evaluation_group_reading_id: evaluationGroupReading.id,
-      },
-    });
-    if (data) {
-      const stats = data.indicadores;
-      await this.prismaService.analysis.create({
-        data: {
-          status: AnalysisStatus.COMPLETED,
-          repetitions_count: stats.cantidad_de_repeticiones,
-          silences_count: stats.cantidad_de_silencios,
-          allosaurus_general_error: stats.error_general_allosaurus,
-          similarity_error: stats.error_similitud,
-          repeated_phonemes: stats.fonemas_repetidos,
-          words_with_errors: stats.palabras_con_errores,
-          words_with_repetitions: stats.palabras_con_repeticiones,
-          score: stats.puntaje,
-          error_timestamps: stats.tiempo_errores,
-          repetition_timestamps: stats.tiempo_repeticiones,
-          phoneme_velocity: Number(stats.velocidad_fonemas) || 0,
-          words_velocity: Number(stats.velocidad_palabras) || 0,
-          raw_analysis: data,
-          recording_id: recording.id,
-        },
-      });
-    }
-
-    return {
-      path,
-      data,
-    };
   }
 }
