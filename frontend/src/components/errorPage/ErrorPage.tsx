f5e9e9396c9e639e6dc91920d27a0679d81d4699
Paolo Mazza <paolo.a.mazza@gmail.com>
2023-12-16 14:14:27 -0300

    Changes background color of loading and error screens when user is student

--- frontend/src/components/errorPage/ErrorPage.tsx
+++ frontend/src/components/errorPage/ErrorPage.tsx
@@ -1,3 +1,4 @@
+import { useIsStudent } from "@/providers/hooks/user";
 import React from "react";
 import styles from "./ErrorPage.module.css";
 
@@ -6,12 +7,22 @@ interface ErrorPageProps {
 }
 
 const ErrorPage: React.FC<ErrorPageProps> = ({ intendedAction }) => {
+  const isStudent = useIsStudent();
   return (
     <div className={`${styles.container}`}>
       {intendedAction && (
         <h1>{`¡Ups! Algo salió mal al ${intendedAction}.`}</h1>
       )}
       {!intendedAction && <h1>¡Ups! Algo salió mal.</h1>}
+      {isStudent && (
+        <style jsx global>
+          {`
+            body {
+              background: var(--ceibal-student-background);
+            }
+          `}
+        </style>
+      )}
     </div>
   );
 };
