42c576c2131f5acfba144935a98c4bd1271a658d
Marcosdo666 <42185858+marcos-do@users.noreply.github.com>
2024-01-02 20:03:52 -0300

    fix select close

--- frontend/src/components/selects/Select.tsx
+++ frontend/src/components/selects/Select.tsx
@@ -14,38 +14,48 @@ export type Option = {
   label: string;
 };
 
+const enterClick = (event: any) => {
+  if (event.key === "Enter") {
+    const element = event.target as HTMLElement;
+    element.click();
+  }
+};
+
 const Select: React.FC<SelectProps> = ({ options, defaultValue, onChange = (value) => {} }) => {
   const divRef = useRef(null);
   const [selectValue, setValue] = useState(defaultValue);
   const [openOptions, setOpen] = useState(false);
 
-  const enterClick = (event: any) => {
-    if (event.key === "Enter") {
-      const element = event.target as HTMLElement;
-      element.click();
-    }
-  };
-
   useEffect(() => {
-    window.addEventListener("click", (e) => {
+    const clickOutSelect = (e: Event) => {
       const element = e.target as HTMLElement;
+
       if (divRef?.current) {
         const divElement = divRef.current as HTMLDivElement;
         if (!divElement.contains(element)) {
+          console.log("HOLA");
           setOpen(false);
         }
       }
-    });
-  }, []);
+    };
+
+    window.addEventListener("click", clickOutSelect);
+    const chakraModal = document.getElementById("chakra-modal-:r9:");
+    chakraModal?.addEventListener("click", clickOutSelect);
+  }, [openOptions]);
 
   return (
     <div
       tabIndex={0}
       className={`${styles["select-bar"]} row`}
-      ref={divRef}
       onKeyDown={enterClick}
+      ref={divRef}
       onClick={() => {
-        setOpen(true);
+        if (!openOptions) {
+          setOpen(true);
+        } else {
+          setOpen(false)
+        }
       }}
     >
       {selectValue.label}
