a586b0ef93858acbd3981160a689cfbb867dd32e
Marcosdo666 <42185858+marcos-do@users.noreply.github.com>
2024-01-06 16:49:20 -0300

    update ui

--- frontend/src/pages/alumno/miprogreso/[evaluacion]/index.tsx
+++ frontend/src/pages/alumno/miprogreso/[evaluacion]/index.tsx
@@ -1,5 +1,11 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from "./evaluacion.module.css";
+import SuarezImage from "@/assets/images/suarez.svg";
+import SecondaryButton from "@/components/buttons/SecondaryButton";
+import PlayIcon from "@/assets/images/play_icon.svg";
+import localFont from "next/font/local";
+import Head from "next/head";
 
 export default function Page() {
   const { evaluacion } = useRouter().query;
@@ -8,9 +14,31 @@ export default function Page() {
   console.log(evaluacion);
 
   return (
-    <div className={styles.container}>
-      <h1>Resultado de evaluación</h1>
-      <h2>El regalo de Oscar</h2>
-    </div>
+    <>
+      <Head>
+        <title>Resultado de Evaluación</title>
+      </Head>
+      <div className={`${styles.container} col`}>
+        <h1>Resultado de Evaluación</h1>
+        <h2>Quiero Ser Suárez</h2>
+        <Image src={SuarezImage} alt="" />
+        <div className={`${styles.info} row`}>
+          <div className="row">
+            <label>Categoría:</label>
+            <span>Fútbol</span>
+          </div>
+          <div className="row">
+            <label>Subcategoría:</label>
+            <span>Intermedio</span>
+          </div>
+        </div>
+        <SecondaryButton variant={"outlined" as keyof Object}>
+          <div>
+            <Image src={PlayIcon} alt=""></Image>
+          </div>
+          <div>Reproducir</div>
+        </SecondaryButton>
+      </div>
+    </>
   );
 }
