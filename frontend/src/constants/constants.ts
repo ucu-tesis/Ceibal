3ed231e9d24f5a6b039cd90dbbd9af70c6f30b2a
Paolo Mazza <paolo.a.mazza@gmail.com>
2024-01-09 18:50:43 -0300

    Adds achievements integration

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -2,11 +2,11 @@ export const tableMaxHeightModal = "300px";
 
 export const notFoundMessage = "No se encontró";
 
-export const bronzeAward = "premio de bronce";
+export const bronzeAchievement = "premio de bronce";
 
-export const silverAward = "premio de plata";
+export const silverAchievement = "premio de plata";
 
-export const goldAward = "premio de oro";
+export const goldAchievement = "premio de oro";
 
 export const inputRegex = /\w|\d|\-|\s/;
 
